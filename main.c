1fa0762b38398edb2fb2e0bf00a10687ddcf06b1
Kevin Amadiva <madivak@live.co.uk>
2018-05-11 18:26:10 +0300

    code cleanup
    code cleanup

--- main.c
+++ main.c
@@ -2,10 +2,10 @@
 /*
  * GPS-UART-BUF.c
  *
- * Created: 10/13/2017 5:37:46 PM
+ * Created: 08/05/2018 5:37:46 PM
  * Author : madiv
  */ 
-#define F_CPU 1000000UL 
+#define F_CPU 1000000UL
 #include <string.h>
 #include <avr/io.h>
 #include <stdio.h>
@@ -21,7 +21,7 @@ char comandoGGA[7] = "$GPGGA";
 int cont=0;
 int bien=0;
 int bien1=0;
-int conta=0; 
+int conta=0;
 char *RMC;
 char *GGA;
 int GPS_position_count=0;
@@ -29,25 +29,29 @@ int datacount=0;
 int datacount1=0;
 int y=0;
 int x=0;
+char status[4];
+char phone[13];
+char *Digits;
 
 #define FOSC 2000000UL // Clock Speed
 #define BAUD 9600
 #define MYUBRR (((FOSC / (BAUD * 16UL))) - 1)
 
 //static FILE uart0_output = FDEV_SETUP_STREAM(USART0_Transmit, NULL, _FDEV_SETUP_WRITE);
+//static FILE uart1_output = FDEV_SETUP_STREAM(USART0_Transmit, NULL, _FDEV_SETUP_WRITE);
 static FILE uart1_output = FDEV_SETUP_STREAM(USART1_Transmit, NULL, _FDEV_SETUP_WRITE);
 static FILE uart1_input = FDEV_SETUP_STREAM(NULL, USART1_Receive, _FDEV_SETUP_READ);
 static FILE uart0_input = FDEV_SETUP_STREAM(NULL, USART0_Receive, _FDEV_SETUP_READ);
 
-int CheckSMS();
-void checkOKstatus();
-int sample_GPS_data (char *GPS0, char *GPS1);
-int grabGPS();
-int CarStatus(int p);
-int sender();
-int CompareNumber();
-int initialstatus();
-int PrintSender();
+void sample_GPS_data (char *GPS0, char *GPS1, char *number);
+void grabGPS();
+unsigned char CheckSMS();
+unsigned char sender();
+unsigned char CompareNumber();
+unsigned char initialstatus();
+void PrintSender();
+void CreateDraft(char m);
+int checkOKstatus(int p);
 
 #define CAR_ON	PORTA |= (1<<PORTA3)
 #define CAR_OFF	PORTA &= ~(1<<PORTA3)
@@ -55,14 +59,11 @@ int PrintSender();
 int a; int i; char w; int y;
 char input;
 char buff[20];
-char company[]	= "+2547XXXXXXXX";// //moha's No#
+char company[]	= "+254700000000";// //moha's No#
 char company2[]	= "+254700000000";// //fatah's no#
-char owner[]	= "+254XXXXXXXXX"; //kevin's no#
+char owner[]	= "+2547XXXXXXXX"; //kevin's no#
+int b;
 
-// ISR(PCINT0_vect)
-// {
-// 	PINA |= (1<<PINA3);
-// }
 int main( void )
 {
 	
@@ -70,7 +71,7 @@ int main( void )
 	
 	USART1_Init(MYUBRR);
 	USART0_Init(MYUBRR);
-	DDRB |= (1<<DDB1); //set PORTB1 as output
+	DDRA |= (1<<DDA3); //set PORTB1 as output
 	
  	stdin = &uart0_input;
  	stdout = &uart1_output; //changed to TX1 for GSM communication. TX0 on Atmega SMD isnt working
@@ -84,16 +85,58 @@ int main( void )
 		stdout = &uart1_output;
 		stdin = &uart1_input;
 		grabGPS();
+//		_delay_ms(2000);
 			
 		fdev_close();
 		stdout = &uart1_output; 
 		stdin = &uart0_input;
+		
 		printf("AT\r\n");
-		_delay_ms(2000);
+	  	checkOKstatus(1);
+	  	//_delay_ms(2000);
 		printf("AT+CMGF=1\r\n");
-		_delay_ms(2000);
+	  	checkOKstatus(1);
+	  	//_delay_ms(2000);
 		printf("AT+CPMS?\r\n");
-			//	_delay_ms(2000);
+	  	checkOKstatus(3);
+	  	//_delay_ms(2000);
+		_delay_ms(1000);
+	}
+	return 0;
+}
+
+int checkOKstatus(int p)
+{
+	b=0;
+	w = getchar();
+	while (w!=0x0A) // skip preceding '/n'
+	{	w = getchar();	}//
+		
+	if (p==1) //if we are only checking for Ok status
+	{
+		w = getchar();
+		if (w==0x04F) // if next character is 'O'
+		{	w = getchar();
+			if (w==0x04B) {	w = getchar();	b=1;} // if w = K
+			else{b=2;}
+		}//
+		else{b=2;}
+	}//
+	else if(p==2)//if we are checking for "SHUT OK"
+	{
+		for (int i=1; i<6;i++) //skip through 1st 5 char of "SHUT OK"
+		{ w = getchar();}
+			
+		w = getchar();
+		if (w==0x04F) // if next character after 'SHUT'is 'O'
+		{	w = getchar();
+			if (w!=0x04B) {	w = getchar();	b=1;} // if w = K
+			else{b=2;}
+		}
+		else{b=2;}
+	}
+	else if(p==3)//if we are checking for new sms
+	{
 			w = getchar();
 			while (w !=  0x2B) //w is not +
 			{ w = getchar();}
@@ -105,30 +148,31 @@ int main( void )
 			}
 			w = getchar();
 			if (w != 0x30)	//if after 7th "," in ("MT",1,75,"SM",1,25,"ME",0,50) next character is not 0
-			  {CheckSMS();}
+			{CheckSMS();}
 			else {}
-			_delay_ms(1000);
 	}
-	return 0;
+	else{}
+	_delay_ms(500);
+ return b;
 }
 
-int CheckSMS()
+unsigned char CheckSMS()
 {
 	int z = 0; //char w;
 	y=0;
 	a=0;
 	printf("AT\r\n");
-	_delay_ms(2000);
+  	checkOKstatus(1);
+  	//_delay_ms(2000);
 	printf("AT+CMGF=1\r\n");
-	_delay_ms(2000);
+  	checkOKstatus(1);
+  	//_delay_ms(2000);
 	printf("AT+CMGL=\"REC UNREAD\"\r\n");
 	while (a < 2) //skip the <LF>
 	{
 		w = getchar();
-		if (w==0x0A)
-		{ a++; }
-		else
-		{}
+		if (w==0x0A) { a++; }
+		else {}
 	}
 	w = getchar();
 	
@@ -140,183 +184,146 @@ int CheckSMS()
 		{ w = getchar();}
 		
 		w = getchar();
-		if (w == 0x030)//w is '0'
-		{	
+		//if ((w == 0x030) || (w == 0x031))//w is '0' or '1'
+		if (w==0x030 || w==0x031)//w is '0' or '1'
+		{
 			CompareNumber();
-			z = buff[14] + buff[15] + buff[16]; //sum values of the 3 buffer values
+			z = status[1] + status[2] + status[3]; //sum values of the 3 buffer values
 			if (z < 3) //A scenario of receiving text from an authorized no# with '1' or '0'
-				{	buff[13] = 0;
-					CAR_OFF; 
-				}
-			else //A scenario of receiving text from Unauthorized no#
-			{buff[13] = 2; }
-			  
-		}
-		else if(w == 0x031)//w is '1'
-		{ 
-			CompareNumber();
-			z = buff[14] + buff[15] + buff[16]; //sum values of the 3 buffer values
-			if (z < 3) //A scenario of receiving text from an authorized no# with '1' or '0'
-			{	buff[13] = 1;
-				CAR_ON;
+			{	status[0] = w;
+				if ( w==0x030)		{CAR_OFF;}
+				else if ( w==0x031)	{CAR_ON;}
+				else{}
+				CreateDraft(w);
 			}
 			else //A scenario of receiving text from Unauthorized no#
-			{buff[13] = 2; }  
+			{	status[0] = 2; printf("AT+CMGD=1,2\r\n"); _delay_ms(2000);} //clearing all SMS in storage AREA except Draft and UNREAD SMS
 		}
-		else{buff[13] = 6; buff[14] = buff[15] = buff[16] = 0; }
+		else{	status[0] = 6; status[1] = status[2] = status[3] = 0; printf("AT+CMGD=1,2\r\n"); _delay_ms(2000);} //clearing all SMS in storage AREA except Draft and UNREAD SMS
 	}
 	else if(w==0x04F) // if w = 'O'
 	{
 		w = getchar();
-		if (w==0x04B) // if w = 'K'
-		{	buff[13] = 3; buff[14] = buff[15] = buff[16] = 0; 
-			//initialstatus();
-		}
+		if (w==0x04B) // if w = 'K'  If there is no new message
+		{	status[0] = 3; status[1] = status[2] = status[3] = 0; printf("AT+CMGD=1,2\r\n"); _delay_ms(2000);}
 		else
-		{ buff[13] = 4; buff[14] = buff[15] = buff[16] = 0; }
-		
+		{	status[0] = 4; status[1] = status[2] = status[3] = 0; printf("AT+CMGD=1,2\r\n"); _delay_ms(2000);}
 	}
 	else
-	{buff[13] = 5; buff[14] = buff[15] = buff[16] = 0; }
-		
-	int E = buff[13];
-		if (E==0) // clear sms storage area if 0/1 is received
-		{
-			printf("AT+CMGD=1,4\r\n"); //clearing all SMS in storage AREA
-			_delay_ms(2000);
-			printf("AT+CMGW=\"");
-			PrintSender();
-			printf("\",145,\"STO UNSENT\"\r\n%d",E);
-			_delay_ms(2000);
-			putchar(0x1A); //putting AT-MSG termination CTRL+Z in USART0
-		}
-		else if (E==1) // clear sms storage area if 0/1 is received
-		{
-			printf("AT+CMGD=1,4\r\n"); //clearing all SMS in storage AREA
-			_delay_ms(2000);
-			printf("AT+CMGW=\"");
-			PrintSender();
-			printf("\",145,\"STO UNSENT\"\r\n%d",E);
-			_delay_ms(2000);
-			putchar(0x1A); //putting AT-MSG termination CTRL+Z in USART0
-		}
-		else
-		{}
-	return *buff;
+	{	status[0] = 5; status[1] = status[2] = status[3] = 0; printf("AT+CMGD=1,2\r\n"); _delay_ms(2000);} 
+
+	return *status;
 }
 
-void checkOKstatus()
+void CreateDraft(char m)
 {
-		w = getchar();
-		while (w!=0x04F) // if w = O
-		{	w = getchar();	}
-			
-		while (w!=0x04B) // if w = K
-		{	w = getchar();	}
+	printf("AT+CMGD=1,4\r\n"); //clearing all SMS in storage AREA
+	checkOKstatus(1);
+	//_delay_ms(2000);
+	printf("AT+CMGW=\"");
+	PrintSender();
+	printf("\",145,\"STO UNSENT\"\r\n%c",m);
+	_delay_ms(2000);
+	putchar(0x1A); //putting AT-MSG termination CTRL+Z in USART0
+	checkOKstatus(1);
+	//_delay_ms(2000);
 }
 
-int CarStatus(int p)
-{
-	int c;
-	c = p;
-	if (c == 1)
-	{ CAR_OFF; }
-	else if (c == 2)
-	{ CAR_ON; }
-	else
-	{}
-	return 0;
-}
-
-int sender()
+unsigned char sender()
 {
 	int n;
 	w = getchar();
 	while (w != 0x02B) // while w is not +
 	{ w = getchar(); }
-	
+
 	for (n=0; n<13; n++) //capture 13 digit phone number
-	{	buff[n] = w;
-	w = getchar();}
+	{	phone[n] = w; w = getchar(); }
 	
-	return *buff;
+	return *phone;
 }
 
-int PrintSender()
+void PrintSender()
 {
 	int n;
-	for (n=1; n<13; n++) //capture 13 digit phone number
-	{	printf("%c", buff[n]);}
-	return 0;
+	for (n=1; n<13; n++) //for 13 digit phone number //something fishy :D if u begin from n=0 we'll have ++ before the no#
+	{	printf("%c", phone[n]);}
 }
 
-int CompareNumber()
+unsigned char CompareNumber()
 {
 	int j;
-	buff[14]=buff[15]=buff[16]=0;
+	status[1] = status[2] = status[3] =0;
 	
 	for (j=0; j<13; j++)
 	{
-		if (buff[j]!=company[j])
-		{ buff[14] = 1;}
+		if (phone[j]!=company[j])
+		{ status[1] = 1;}
 		else{}
-		if (buff[j]!=owner[j])
-		{ buff[15] = 1;}
+		if (phone[j]!=owner[j])
+		{ status[2] = 1;}
 		else{}
-		if (buff[j]!=company2[j])
-		{ buff[16] = 1;}
+		if (phone[j]!=company2[j])
+		{ status[3] = 1;}
 		else{}
 	}
-	return *buff;
+	return *status;
 }
 
-int sample_GPS_data (char *GPS0, char *GPS1)
+void sample_GPS_data (char *GPS0, char *GPS1, char *number)
 {
 
 	//int i=0;
 	printf("AT\r\n");
-	_delay_ms(1000);
+  	checkOKstatus(1);
+	//_delay_ms(2000);
 	printf("AT+CGATT=1\r\n");
-	_delay_ms(2000);
+  	checkOKstatus(1);
+  	//_delay_ms(2000);
 	printf("AT+CIPMUX=0\r\n");
-	_delay_ms(1000);
+  	checkOKstatus(1);
+  	//_delay_ms(2000);
 	printf("AT+CSTT=\"APN\",\"\",\"\"\r\n");
-	_delay_ms(2000);
+  	checkOKstatus(1);
+  	//_delay_ms(2000);
 	printf("AT+CIICR\n");
-	printf("\r\n");
-	_delay_ms(3000);
+	//printf("\r\n");
+  	checkOKstatus(1);
+  	_delay_ms(2000);
 	printf("AT+CIFSR\r\n");
-	_delay_ms(2000);
+	_delay_ms(3000);
 	printf("AT+CIPSTART=\"TCP\",\"IP\",\"PORT\"\r\n");
-	_delay_ms(2000);
+	_delay_ms(3000);
 	printf("AT+CIPSEND\r\n");
 	_delay_ms(1000);
-
-	//printf("#CAR PLATE NO:[KCQ 450R]\r\r%s\r%s\r\nBUFF[13] = %d\r\r\nCar status is = %d\r\r\nText no# is :0727XXXXXX\r\r\n",GPS0,GPS1,buff[13],buff[13]); //MOHA
-	printf("#CAR PLATE NO:[KCP 213S]\r\r%s\r%s\r\nBUFF[13] = %d\r\r\nCar status is = %d\r\r\nText no# is :0727XXXXXX\r\r\n",GPS0,GPS1,buff[13],buff[13]); //KEVIN
-	_delay_ms(2500);
-
-// 	PrintSender();
-// 	printf("\r\n");
+	//printf("#CAR PLATE NO:[KCQ 451R]\r\r%s\r%s\r\nBUFF[13] = %c\r\r\nCar status is = %c\r\r\nText no# is :%s\r\r\n",GPS0,GPS1,status[0],status[0],number); //MOHA
+	printf("#CAR PLATE NO:[KCP 213S]\r\r%s\r%s\r\nBUFF[13] = %c\r\r\nCar status is = %c\r\r\nText no# is :%s\r\r\n",GPS0,GPS1,status[0],status[0],number); //KEVIN
+	_delay_ms(500);
 	putchar(0x1A); //putting AT-MSG termination CTRL+Z in USART0
-	_delay_ms(2000);
+	//checkOKstatus(2);
+	_delay_ms(3000);
 	printf("AT+CIPCLOSE\r\n");
-	_delay_ms(2000);
+  	checkOKstatus(1);
+  	//_delay_ms(2000);
 	printf("AT+CIPSHUT\r\n");
-	_delay_ms(2000);
-	return 0;
+  	checkOKstatus(2);
+  	//_delay_ms(2000);
+	//return 0;
 }
 
-int initialstatus()
+unsigned char initialstatus()
 {
 	//char w;
 	y=0;
 	a=0;
 	printf("AT\r\n");
+  	//checkOKstatus(1);
+  	_delay_ms(2000);
+	putchar(0x1A); //putting AT-MSG termination CTRL+Z in USART just in case it hangs at message or TCP sending
 	_delay_ms(2000);
 	
 	printf("AT+CMGF=1\r\n");
-	_delay_ms(2000);
+	checkOKstatus(1);
+	//_delay_ms(2000);
 	
 	printf("AT+CPMS=\"MT\",\"SM\",\"ME\"\r\n");
 	_delay_ms(2000);
@@ -334,7 +341,8 @@ int initialstatus()
 	w = getchar();
 	if (w != 0x30)	{CheckSMS();}
 	else {}	
-	_delay_ms(2000);	
+		_delay_ms(500);
+	//_delay_ms(2000);	
 	printf("AT+CMGR=1\r\n");
 	while (a < 2) //skip the <LF>
 	{
@@ -349,38 +357,31 @@ int initialstatus()
 	if (w==0x02B) // if w = +
 	{
 		sender();
-		
 		w = getchar();
 		while (w !=  0x0A) //w is not <LF>
 		{ w = getchar();}
 		
 		w = getchar();
-		if (w == 0x030)//w is '0'
-		{ buff[13] = 1; _delay_ms(200); CAR_OFF; }
-		else if(w == 0x031)//w is '1'
-		{ buff[13] = 2; _delay_ms(200); CAR_ON;  }
-		else{
-			buff[13] = 6;
-// 			printf("AT+CMGD=1,4\r\n"); //clearing all SMS in storage AREA
-// 			_delay_ms(2000);
-			}
+		if (w==0x030 || w==0x031)//w is '0' or '1'
+		{	status[0] = w; 
+			_delay_ms(200); 
+			if (w==0x030)		{CAR_OFF;}
+			else if (w==0x031)	{CAR_ON;}
+			else{}
+		}			
+		else{ status[0] = 6; }
 	}
-	
-	else
-	{
-// 		printf("AT+CMGD=1,4\r\n"); //clearing all SMS in storage AREA
-// 		_delay_ms(2000);
-	}
+	else{}
 
-	return *buff;
+	return *status;
 }
 
 
-int grabGPS()
+void grabGPS()
 {
-y=0; x=0;
-while(x==0 || y==0)
-{
+  y=0; x=0;
+  while(x==0 || y==0)
+	{
 	byteGPS=getchar();
 	// Read a byte of the serial port
 	if (byteGPS == -1)
@@ -432,10 +433,14 @@ while(x==0 || y==0)
 
 			if (x==1 && y==1)
 			{
+				Digits = (char*) malloc(14);//allocate memory for the phone number that text the tracker 
+				strcpy(Digits,phone);		//save the number that texted to a char string
+				
 				_delay_ms(500);
-				sample_GPS_data(GGA,RMC);
-				free(GGA); //The memory loaction pointed by GGA is freed. Otherwise it will cause error
-				free(RMC); //The memory loaction pointed by RMC is freed. Otherwise it will cause error
+				sample_GPS_data(GGA,RMC,Digits);
+				free(GGA); //The memory locaction pointed by GGA is freed. Otherwise it will cause error
+				free(RMC); //The memory locaction pointed by RMC is freed. Otherwise it will cause error
+				free(Digits);
 				_delay_ms(2000);
 			}
 			else{}
@@ -451,6 +456,7 @@ while(x==0 || y==0)
 		} //byteGPS==13
 	}  //else byteGPS is not null
 } //Serial1.available
-return 0;
+//return 0;
 } //testGPS
+
 
